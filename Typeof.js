c74ee710340d6a7d4ed2e8ac0a0fd2288154974c
YadBro <ncada708@gmail.com>
2021-04-28 05:40:45 +0700

    typeof javascript

---
+++ Typeof.js
@@ -0,0 +1,14 @@
+// **TYPEOF**
+
+/*
+
+    typeof ini untuk mengidentifikasi atau mencari tahu tipe data dari nilai
+
+*/
+
+console.log(typeof(10))
+console.log(typeof(true))
+console.log(typeof("Hi Everyone"))
+console.log(typeof(["Testing"]))
+console.log(typeof(undefined))
+console.log(parseFloat("He was 40"))
