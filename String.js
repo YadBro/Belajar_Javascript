b9c3166ce4099d6f79c6d863419742944e3909b1
YadBro <ncada708@gmail.com>
2021-04-28 01:34:59 +0700

    Commit Pertama

---
+++ String.js
@@ -0,0 +1,11 @@
+/* STRING
+var nama = "Yadi Apriyadi";
+var print = "Hello " + nama + "Apa kabar?";
+*/
+
+// Tes 1
+var namaPendek = "Yadi";
+var namaPanjang = "Apriyadi"
+var print = string1 += string2;
+
+console.log(print)
