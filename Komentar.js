d8628671fcb7cecb9b448f544c0cec7671689b16
YadBro <ncada708@gmail.com>
2021-04-28 02:19:48 +0700

    Komentar dalam javascript

--- Komentar.js
+++ Komentar.js
@@ -1,1 +1,35 @@
-var
+
+// COMMENT ADALAH SEBUAH Komentar yang memiliki tujuan yang banyak seperti menandari baris coding ini untuk menjalankan apa,dsb.
+
+
+var in_LineComment = "Ini in line comment!"; // in-line comment
+
+
+var multi_comment = "Ini multi comment!"
+/* Ini multi comment */
+
+/*
+ds
+as
+sd
+s
+ds
+*/
+
+var testing = "INI TESTING!"
+// var tes
+/* var tes */
+
+
+/* Keterangan:
+
+     [ // ] <- ini adalah in line comment, comment ini hanya bisa dalam satu baris saja
+
+*/
+
+//  [ /* */ ]  <- ini adalah multi line comment, comment ini bisa dalam beberapa baris sampai dia di tutup,
+//             jadi untuk menggunakan comment ini kita perlu membuka dengan /* dan menutup dengan */
+
+
+
+
