f31b674e348c795b2c2d4bedad9b5ca4b6f0dd96
YadBro <ncada708@gmail.com>
2021-04-28 06:30:35 +0700

    initial commit

---
+++ Tipe_Data_String.js
@@ -0,0 +1,52 @@
+// Tipe Data String dalam javascript digunakan untuk mengepresikan data tekstual
+
+/*
+    Dalam pembuatan string bisa dilakukan dengan:
+    [1]  Double Quote = " "
+    [2]  Single Quote = ' '
+*/
+
+
+var contohString1 = '"(double quote)Saya adalah seorang programmer professional"'
+var contohString2 = "'(single quote)Saya adalah seorang programmer professional'"
+console.log(contohString1)
+console.log(contohString2)
+console.log("\n")
+
+/*
+
+    Penulisan salah string:
+     "Kegitan pungut sampah dilakukan pada hari jum'at"
+     'Kegitan pungut sampah dilakukan pada hari jum"at'
+
+    Apabila ingin menambahkan quote yang berbeda di perlukan escape character!
+    terdiri dari:
+
+    [1] \0  <-Hasil: Null
+    [2] \'  <-Hasil: '  (Menambahkan single quote string pada double quote string)
+    [3] \"  <-Hasil: "  (Menambahkan double quote string pada single quote string)
+    [4] \\  <-Hasil: \  (Menambahkan \ pada double quote string dan juga single qoute string)
+    [5] \n  <-Hasil: new line/baris baru  (Membuat baris baru dalam string)
+    [6] \t  <-Hasil: tab (Membuat jarak agak jauh dalam string)
+    [7] \b  <-Hasil: \b (Menghapus / backspace)
+    [8] \uxxxx <-Hasil: unicode
+
+
+*/
+
+// **CONTOH**
+
+console.log("ini adalah Null(tidak ada apa-apa) \0")
+console.log("ini adalah menambahkan single quote (Jum\'at)")
+console.log("ini adalah menambahkan double quote (Jum\"at)")
+console.log("ini adalah menambahkan garis miring (5\\6)")
+console.log("ini adalah new line\\baris baru \n hey aku ada di bawah!")
+console.log("ini adalah jarak \t jauh")
+console.log("ini adalah jarak \b dekat")
+
+// Memasukkan variable ke tengah tengah character tanpa penghubung +
+
+// syarat penggunaan menggunakan ``, $, dan {}
+
+
+console.log(`ini contoh string 1= ${contohString1}`)
