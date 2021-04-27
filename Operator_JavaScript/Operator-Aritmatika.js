1ff30d3fdc8bab9d865661f981b938db89ef92f3
YadBro <ncada708@gmail.com>
2021-04-28 03:03:57 +0700

    Pelajar Baru dan Di perbaharui

---
+++ Operator_JavaScript/Operator-Aritmatika.js
@@ -0,0 +1,25 @@
+/* Dalam Operator Aritmatika ada: [ + - * / ]
+
+Keterangan:
+    +   <-Tambah
+    -   <-Kurang
+    *   <-Kali
+    /   <-Bagi
+
+
+*/
+
+// Contoh
+
+var Lima = 5;
+var Tiga = 3;
+var HasilTambah = Lima + Tiga
+var HasilKurang = Lima - Tiga
+var HasilKali = Lima * Tiga
+var HasilBagi = Lima / Tiga
+
+// **TAMBAH**
+console.log("[TAMBAH]   "+Lima+" + "+Tiga+"   Hasilnya adalah= "+HasilTambah)
+console.log("[KURANG]   "+Lima+" - "+Tiga+"   Hasilnya adalah= "+HasilKurang)
+console.log("[KALI]   "+Lima+" * "+Tiga+"   Hasilnya adalah= "+HasilKali)
+console.log("[BAGI]   "+Lima+" / "+Tiga+"   Hasilnya adalah= "+HasilBagi)
--- String.js
+++ String.js
@@ -5,7 +5,11 @@ var print = "Hello " + nama + "Apa kabar?";
 
 // Tes 1
 var namaPendek = "Yadi";
-var namaPanjang = "Apriyadi"
-var print = string1 += string2;
+var namaPanjang = "Apriyadi";
+var gabunganString = namaPendek + namaPanjang;
+var gabunganString_dengan_Angka = "Angkanya adalah= "+ 3;
 
-console.log(print)
+console.log(gabunganString)
+console.log(gabunganString_dengan_Angka)
+
+// Untuk menggabungkan string dengan apapun itu gunakan simbol tambah
