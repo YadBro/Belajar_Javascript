cca930baae872e0ac609e1a2b93b386b16917c90
YadBro <83240362+YadBro@users.noreply.github.com>
2022-02-27 08:30:16 +0700

    Update Komentar.js

--- Komentar.js
+++ Komentar.js
@@ -24,11 +24,11 @@ var testing = "INI TESTING!"
 /* Keterangan:
 
      [ // ] <- ini adalah in line comment, comment ini hanya bisa dalam satu baris saja
-
+     
 */
 
 //  [ /* */ ]  <- ini adalah multi line comment, comment ini bisa dalam beberapa baris sampai dia di tutup,
-//             jadi untuk menggunakan comment ini kita perlu membuka dengan /* dan menutup dengan */
+//                jadi untuk menggunakan comment ini kita perlu membuka dengan /* dan menutup dengan */
 
 
 
