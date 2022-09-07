c7f961fc097619f648ea64533fac3d4241c9bc67
YadBro <ncada708@gmail.com>
2022-09-08 03:38:17 +0700

    escape string

---
+++ escape_string_chars.js
@@ -0,0 +1,9 @@
+console.log("We Are \'Vikings\'"); // single quote
+console.log("We Are \"Vikings\""); // double quote
+console.log("We Are \\Vikings\\"); // backslash
+console.log("We Are \bVikings\b"); // backspace
+console.log("We Are \fVikings\f"); // form feed
+console.log("We Are \nVikings\n"); // New Line
+console.log("We Are \rVikings\r"); // Carriege Return
+console.log("We Are \tVikings\t"); // Horizontal Tabulator
+console.log("We Are \vVikings\v"); // Vertical Tabulator
